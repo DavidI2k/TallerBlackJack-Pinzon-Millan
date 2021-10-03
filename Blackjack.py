a47cb2d7f5af5fc48311d13308f02d06c1e940a9
DavidI2k <80795851+DavidI2k@users.noreply.github.com>
2021-10-03 11:11:05 -0500

    update

--- Blackjack.py
+++ Blackjack.py
@@ -4,7 +4,7 @@ class Card:
         self.symbol = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'][symbol-1]
         self.suit = '♥♦♣♠'[suit]
 
-    def show(self):
+    def mostrar(self):
         print('┌───────┐')
         print(f'| {self.symbol:<2}    |')
         print('|       |')
@@ -13,7 +13,7 @@ class Card:
         print(f'|    {self.symbol:>2} |')
         print('└───────┘') 
 
-    def score(self):
+    def puntaje(self):
         if self.cost >= 10:
             return 10
         elif self.cost == 1:
